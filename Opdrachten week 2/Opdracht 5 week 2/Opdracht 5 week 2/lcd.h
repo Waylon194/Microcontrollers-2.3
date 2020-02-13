afe2f36ee0acd009267443bccf0c9ebfe2f90246
Waylon <waylon194@hotmail.com>
2020-02-13 12:28:21 +0100

    Updated opdracht 5 week 2
    Adjusted the code to match the requirements.

--- Opdrachten week 2/Opdracht 5 week 2/Opdracht 5 week 2/lcd.h
+++ Opdrachten week 2/Opdracht 5 week 2/Opdracht 5 week 2/lcd.h
@@ -1,16 +1,14 @@
-
 /*
  * lcd.h
  *
  * Created: 6-2-2020 13:05:31
  *  Author: Waylon
- */ 
+ */
 
-void init();
-void display_text(char *str);
+void init_4bits_mode(void);
+void lcd_write_text(char *str, int line, _Bool slowmode);
+void lcd_clear(void);
+void lcd_strobe_lcd_e(void);
+void lcd_write_data(unsigned char byte);
+void lcd_write_command(unsigned char byte);
 void set_cursor(int position);
-
-/*
-	Met behulp van de simulator (deel A) heb je geleerd hoe het LCD werkt.
-	Verwerk deze in de body van bovenstaande functies.
-*/
--- Opdrachten week 2/Opdracht 5 week 2/Opdracht 5 week 2/main.c
+++ Opdrachten week 2/Opdracht 5 week 2/Opdracht 5 week 2/main.c
@@ -1,26 +1,26 @@
-/*
- * Opdracht 5 week 2.c
- *
- * Created: 6-2-2020 13:03:10
- * Author : Waylon
- */ 
-
 #include <avr/io.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
 
-/*
-	Implementeer (in een aparte file) een test programma om bovenstaande te testen.
-	Als alles goed werkt heb je nu een generieke module gemaakt die je in al je projecten kunt gebruiken.
-*/
+#define false 0
+#define true 1
 
 int main(void)
 {
-    while (1) 
-    {
-		testLCDScreen();
-    }
-}
+	// Init I/O
+	DDRD = 0xFF; // PORTD(7) output, PORTD(6:0) input
 
-void testLCDScreen(){
-	//TODO: Make a test method for the LCD screen
-}
+	// Init LCD
+	init_4bits_mode();
 
+	while (1)
+	{
+		lcd_clear();
+		lcd_write_text("Yedi you are", 1, false);
+		lcd_write_text("Yedi you are", 2, true);
+		//Call to trigger error message on LCD screen.
+		//lcd_write_text("Yedi you are", 3, true);
+	}
+}
